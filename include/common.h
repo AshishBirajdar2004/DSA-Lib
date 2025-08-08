48f425d5d036081f466d3f324bc0e1621fd7ff62
Ashish Birajdar <birajdarashish2020@gmail.com>
2025-08-08 10:06:46 +0530

    feat(include): add common header file to generalise execution status for all data structures

---
+++ include/common.h
@@ -0,0 +1,42 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+/**
+ * Generic status codes for data structure operations
+ */
+typedef enum {
+    STATUS_OK = 0,
+    STATUS_ERR_ALLOC = 1,
+    STATUS_ERR_INVALID_ARGUMENT = 2,
+    STATUS_ERR_DUPLICATE_KEY = 3,
+    STATUS_ERR_KEY_NOT_FOUND = 4,
+    STATUS_ERR_UNDERFLOW = 5,
+    STATUS_ERR_OVERFLOW = 6,
+    STATUS_ERR_EMPTY = 7,
+    STATUS_ERR_FULL = 8,
+    STATUS_ERR_UNKNOWN = 100
+} STATUS;
+
+/**
+ * Status struct for insert operations
+ */
+typedef struct {
+    bool isInserted;
+    bool isDuplicate;
+    bool isAllocFailed;
+} InsertStatus;
+
+/**
+ * Status struct for delete operations
+ */
+typedef struct {
+    bool isDeleted;
+    bool isKeyNotFound;
+} DeleteStatus;
+
+#endif // COMMON_H
